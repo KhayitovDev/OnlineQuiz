31c24c4ffbfc0c817e841f9b405e4c9cd831e644
KhayitovDev <khayitovkhayitboy2109@gmail.com>
2023-04-27 17:39:35 +0500

    created user model

--- online_quiz/models.py
+++ online_quiz/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    first_name=models.CharField(max_length=255, null=False, blank=False)
+    last_name=models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.first_name
+    
+    class Meta:
+        verbose_name='User'
+        verbose_name_plural='Users'
+
+    
+
